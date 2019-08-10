1725105f2fd293d6e0804f7ed83049346326743f
Isuru Liyanage <isuruliyanage1@gmail.com>
2019-08-10 21:54:19 +0530

    bitwise,mic count method added

--- src/Cs/cs_java.java
+++ src/Cs/cs_java.java
@@ -8,22 +8,29 @@ public class cs_java {
 	ArrayList<String> lines;
 	ArrayList<Integer> CsUnit;
 
-	// Expressions for checking if conditions
+	// Expressions for checking Keywords
 	String keyword_regx = "\\b(?!public |static |else |else\\{|try |try\\{|return )[\\d\\w]+";
 	// Expression for checking the special words throw,throws,new,delete
 	String special_reg = "\\b(new |delete |throw |throws |throw\\{|throws\\{)";
-	// Expression for checking the relational operators 
-	String relation_reg ="\\b(==| == |!=| != |>=| >= |<=| <= |>| > |<| < )";
+	// Expression for checking the relational operators
+	String relation_reg = "\\b\\w\\s*(==| == |!=| != |>=| >= |<=| <= |>| > |<| < )\\s*\\w";
 	// Expression for checking the Arithmetic operators
-	String arithmetic_reg = "\\b(\\++| \\++ |--| -- |\\*| \\* |\\/| \\/ |%| % |\\+| \\+ |\\-| \\-)";
-	// Expression for checking the 
+	String arithmetic_reg = "\\b\\w\\s*(\\++| \\+\\+ |\\-\\-| \\-\\- |\\*| \\* |\\/| \\/ |\\%| \\% |\\+| \\+ |\\-| \\-)\\s*\\w";
+	// Expression for checking the Logical operators
+	String logical_reg = "\\b(&&| \\&& |\\|\\|| \\|\\| |!| ! )";
+	// Expression for checking the Bitwise operators
+	String bitwise_reg = "\\b\\w\\s*(\\^| \\^ |\\|| \\| |\\~| \\~ |\\<\\<\\<| <<< |\\>\\>\\>| >>> |\\>\\>| \\>\\> |\\<\\<| \\<\\< )\\s*\\w";
+	// Expression for checking the Miscellaneous operators
+	String mic_reg = "\\b\\w\\s*(\\,| \\, |\\-\\>| \\-\\> |\\.| \\. |\\:\\:| \\:\\: )\\s*\\w";
+	// Expression for checking the Assignment operators
+	String ass_reg = "\\b\\w\\s*(\\>\\>\\>\\=| \\>\\>\\>\\= |\\<\\<\\=| \\<\\<\\= |\\>\\>\\=| \\>\\>\\= |\\^\\=| \\^\\= |\\|\\=| \\|\\= |\\&\\=| \\&\\= |\\%\\=| \\%\\= |\\/\\=| \\/\\= |\\*\\=| \\*\\= |\\-\\=| \\-\\= |\\+\\=| \\+\\= |\\=| \\= )\\s*\\w";
 
 	public cs_java(ArrayList<String> lines) {
 		this.lines = lines;
 		CsUnit = new ArrayList<Integer>(lines.size());
 	}
 
-	//this method will count the keywords in the code 
+	// this method will count the keywords in the code
 	public int keyword_count(String line) {
 
 		Pattern pattern = Pattern.compile(keyword_regx);
@@ -33,9 +40,10 @@ public class cs_java {
 		while (matcher.find()) {
 			count++;
 		}
-		
+
 		return count;
 	}
+
 	public int special_count(String line) {
 
 		Pattern pattern = Pattern.compile(special_reg);
@@ -44,11 +52,12 @@ public class cs_java {
 		int count = 0;
 		while (matcher.find()) {
 			count++;
-			
+
 		}
-		
+
 		return count;
 	}
+
 	public int relation_count(String line) {
 
 		Pattern pattern = Pattern.compile(relation_reg);
@@ -57,11 +66,12 @@ public class cs_java {
 		int count = 0;
 		while (matcher.find()) {
 			count++;
-			
+
 		}
-		
+
 		return count;
 	}
+
 	public int arithmatic_count(String line) {
 
 		Pattern pattern = Pattern.compile(arithmetic_reg);
@@ -70,13 +80,66 @@ public class cs_java {
 		int count = 0;
 		while (matcher.find()) {
 			count++;
-			
+
 		}
-		
+
 		return count;
 	}
-	
-	
+
+	public int logical_count(String line) {
+
+		Pattern pattern = Pattern.compile(logical_reg);
+		Matcher matcher = pattern.matcher(line);
+
+		int count = 0;
+		while (matcher.find()) {
+			count++;
+
+		}
+
+		return count;
+	}
+
+	public int bitwise_count(String line) {
+
+		Pattern pattern = Pattern.compile(bitwise_reg);
+		Matcher matcher = pattern.matcher(line);
+
+		int count = 0;
+		while (matcher.find()) {
+			count++;
+
+		}
+
+		return count;
+	}
+
+	public int mic_count(String line) {
+
+		Pattern pattern = Pattern.compile(mic_reg);
+		Matcher matcher = pattern.matcher(line);
+
+		int count = 0;
+		while (matcher.find()) {
+			count++;
+
+		}
+
+		return count;
+	}
+	public int ass_count(String line) {
+
+		Pattern pattern = Pattern.compile(ass_reg);
+		Matcher matcher = pattern.matcher(line);
+
+		int count = 0;
+		while (matcher.find()) {
+			count++;
+
+		}
+
+		return count;
+	}
 
 	public void addtoArray() {
 		for (int i = 0; i < lines.size(); i++) {
@@ -84,8 +147,13 @@ public class cs_java {
 			int special_count = special_count(lines.get(i));
 			int relation_count = relation_count(lines.get(i));
 			int arithmatic_count = arithmatic_count(lines.get(i));
-			CsUnit.add(keyword_count + special_count + relation_count + arithmatic_count);
-			
+			int logical_count = logical_count(lines.get(i));
+			int bitwise_count = bitwise_count(lines.get(i));
+			int mic_count = mic_count(lines.get(i));
+			int ass_count = ass_count(lines.get(i));
+			CsUnit.add(keyword_count + special_count + relation_count + arithmatic_count + logical_count + bitwise_count
+					+ mic_count + ass_count);
+
 		}
 	}
 
