468b7c18ef2dbd6e55267584a035e9c283bc460f
Sarani <saranipeiris17@gmail.com>
2019-08-20 08:40:28 +0530

    Added Ci files

---
+++ src/Ci/Ci_java.java
@@ -0,0 +1,13 @@
+package Ci;
+
+import java.util.ArrayList;
+
+// Measuring complexity due to inheritance
+public class Ci_java() {
+
+    ArrayList<String> lines;
+    ArrayList<Integer> Ci; 
+
+
+
+}
